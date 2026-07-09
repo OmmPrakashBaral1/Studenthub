25d628d46cbe73b6851366ba09c581a55e66737f
Omm Prakash Baral <ommprakashbaral77@gmail.com>
2026-07-09 08:23:25 +0530

    Deploy Flask app to ECS

--- app.py
+++ app.py
@@ -1,11 +1,28 @@
 from flask import Flask, render_template
-import psycopg2, os
+import psycopg2
+import os
 
 app = Flask(__name__)
 
+def check_db_status():
+    try:
+        conn = psycopg2.connect(
+            host=os.environ.get('DB_HOST'),
+            dbname=os.environ.get('DB_NAME'),
+            user=os.environ.get('DB_USER'),
+            password=os.environ.get('DB_PASSWORD'),
+            port=os.environ.get('DB_PORT', 5432),
+            connect_timeout=3
+        )
+        conn.close()
+        return "Connected"
+    except Exception as e:
+        return f"Not connected ({e})"
+
 @app.route('/')
 def home():
-    return render_template('index.html', message="Studenthub is live on AWS!")
+    db_status = check_db_status()
+    return render_template('index.html', message='Hello, World!', db_status=db_status)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0',port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
